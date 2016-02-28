13dfe0649be4485395f0b9453c3e900341663bcb
Benjamin Lannon <lannonbr@clarkson.edu>
2016-02-28 11:55:55 -0500

    Fixed a few things in the gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,17 @@ var gulp = require('gulp'),
   browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src("./sass/main.scss")
-    .pipe(sass({outputStyle: 'expanded'}))
+  return gulp.src("./sass/app.scss")
+    .pipe(plugins.sass({outputStyle: 'expanded'}))
     .pipe(gulp.dest("./css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task('jade', function() {
   return gulp.src("./jade/*.jade")
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest("./"));
+    .pipe(plugins.jade({pretty: true}))
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
